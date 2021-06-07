e41e4003ebbeffa349dc8990218571515cc88acb
Queen-01 <quienzy.ongeye@student.moringaschool.com>
2021-06-07 10:17:09 +0300

    add files

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,5 +6,11 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
-
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
 }
