782e2bd2ee1e099a56eba40eb235b0d4f56a806c
Queen-01 <quienzy.ongeye@student.moringaschool.com>
2021-06-07 20:19:56 +0300

    add changes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,8 +6,23 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
+    private static int Port;
+
     public static void main(String[] args) {
+        ProcessBuilder process = new ProcessBuilder();
+            Integer port = null;
+           if (process.environment().get("PORT") != null){
+                Port = Integer.parseInt(process.environment().get("PORT"));
+            }else{
+               port = 4567;
+            }
+           port(port);
+
         staticFileLocation("/public");
+        Hero.setUpNewHero();
+        Hero.setUpNewHero1();
+//        Squad.setUpNewSquad();
+
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             return new ModelAndView(model, "index.hbs");
